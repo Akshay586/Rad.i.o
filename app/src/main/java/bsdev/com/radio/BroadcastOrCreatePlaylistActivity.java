d0a67bea3ccb27c48611f953d0f3b4ebe939fb1d
Akshay <Akshay.Rathore06@gmail.com>
2018-01-28 16:28:04 +0530

    Added BroadcastOrCreatePlaylist Activity

---
+++ app/src/main/java/bsdev/com/radio/BroadcastOrCreatePlaylistActivity.java
@@ -0,0 +1,17 @@
+package bsdev.com.radio;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Akshay on 28-01-2018.
+ */
+
+public class BroadcastOrCreatePlaylistActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
